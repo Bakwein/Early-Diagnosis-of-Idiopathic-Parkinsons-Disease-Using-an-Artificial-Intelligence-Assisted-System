3389401a53ef5a7db4b59c5221d43c7710a3f0dd
Efe Yılmaz Taşyürek <138163679+efeyilmaz25@users.noreply.github.com>
2025-10-04 20:57:06 +0300

    Update Data_preparation.py
    final regulations

--- Face Modality Module/Modeling/Data_preparation.py
+++ Face Modality Module/Modeling/Data_preparation.py
@@ -14,8 +14,6 @@ import seaborn as sns
 import cv2
 import time 
 
-
-
 # Configure Detector
 detector = Detector(
     face_model="retinaface",
@@ -24,8 +22,6 @@ detector = Detector(
     emotion_model="resmasknet",
     facepose_model="img2pose",
 )
-
-
 
 au_means = pd.DataFrame(columns=['name'])  
 #all AU
@@ -38,13 +34,9 @@ au_columns = [
 ]
 
 
-
-
 ########################################################################function1 
 def extract_name_from_path(video_path):
-    
     if "dataset" in video_path:
-        
         dataset_index = video_path.index("Data_Hicbirkirpmayok") + len("Data_Hicbirkirpmayok")
         relevant_path = video_path[dataset_index:]
 
@@ -55,67 +47,40 @@ def extract_name_from_path(video_path):
         else:
             name = relevant_path.strip('/')
     else:
-        
         name = video_path
 
     return name
 
 
-
-
-
-
-
-
 ########################################################################function2
 def process_video(video_path):
-    
     predictions = detector.detect_video(video_path, skip_frames=1)  # skip_frames=1 
-
-
     au_data = predictions[au_columns] if all(col in predictions.columns for col in au_columns) else predictions
 
-     
     for column in au_data.columns:
         au_data[column] = pd.to_numeric(au_data[column], errors='coerce')
 
-
-    
     return au_data.mean().to_frame().T 
-
-
-
-
 
 
 ########################################################################function3
 def process_video_with_fallback(video_path, au_columns):
-    
     au_means = process_video(video_path)
-
     
     for au in au_columns:
         if au not in au_means.columns:
-            au_means[au] = 0.0  
+            au_means[au] = 0.0
 
-    
-    return au_means[au_columns]  
-
-
-
+    return au_means[au_columns]
 
 
 ########################################################################function4
 def get_video_info(video_path):
-    
     cap = cv2.VideoCapture(video_path)
-
-    
     if not cap.isOpened():
         print("Failed to open video!")
         return None
 
-   
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # FPS (Frames Per Second) 
@@ -124,76 +89,50 @@ def get_video_info(video_path):
     # time calculate
     duration = total_frames / fps  
 
-    
     print(f"Total Frame Count: {total_frames}")
     print(f"FPS (Frames per Second): {fps}")
     print(f"Duration (seconds): {duration:.2f}")
 
-    
     cap.release()
 
-
     return total_frames, fps, duration
-
-
-
-
 
 
 ########################################################################function5
 def process_videos_in_batches(video_paths, output_prefix,grup):
     # Start timer at the beginning of each video
     start_time = time.perf_counter()
-   
     for i, video_path in enumerate(video_paths):
         df_results = pd.DataFrame()  
-
-        
         total_frames, fps, duration = get_video_info(video_path)
-
-        
         au_means = process_video_with_fallback(video_path, au_columns)
-
-
-
+        
         # new columns add
         au_means.loc[:, 'total_frame'] = total_frames
         au_means.loc[:, 'fps'] = fps
         au_means.loc[:, 'time'] = duration
 
-
         # Timer finish
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
 
-
         # I converted the time in seconds to minutes
         elapsed_minutes = elapsed_time / 60
-
-
+        
         au_means.loc[:, 'videoPro_timedk'] = round(elapsed_minutes, 3)  
         au_means.loc[:, 'name'] = extract_name_from_path(video_path)
-
-
 
         if(output_prefix=="healthy"):
           au_means.loc[:, 'label'] = 0  # Healthy individual
         else:
           au_means.loc[:, 'label'] = 1  # sick individual
 
-      
         df_results = pd.concat([df_results, au_means], ignore_index=True)
 
-        
         if not df_results.empty:
-           
             output_excel_path = f'<project_root>/dataset_test/{output_prefix}_{grup}_{i}.xlsx'#THIS WILL BE CHANGED ACCORDING TO THE APPROPRIATE DATASET PATH
             df_results.to_excel(output_excel_path, index=False)
             print(f"{output_excel_path} has been saved.")
-
-
-
-
 
 
 ####################################################################################################
@@ -201,6 +140,3 @@ def process_videos_in_batches(video_paths, output_prefix,grup):
 
 pathlist=["<project_root>/test/parkinson/sample-patient-name-durus-tekayak-trim_YQ3k7X3f.mp4"] #Enter the video paths to be processed with py feat here
 process_videos_in_batches(pathlist, 'parkinson',0)
-
-
-
